5f63a0530a6464a81e132a15aa7415815e6080e8
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-10-21 12:23:09 +0200

    FEN parser

---
+++ engine/engineutils/strings.go
@@ -0,0 +1,26 @@
+package engineutils
+
+import (
+	"errors"
+	"fmt"
+)
+
+// ex: a1 -> 0, g8 -> 63
+func CoordStringToIdx(coord *string) (uint16, error) {
+	if len(*coord) != 2 {
+		return 0, errors.New("Invalid coordinates " + *coord)
+	}
+
+	col := (*coord)[0] - 'a'
+	row := (*coord)[1] - '1'
+	if col < 0 || col > 7 || row < 0 || row > 7 {
+		return 0, errors.New("Invalid coordinates " + *coord)
+	}
+	return uint16(row*8 + col), nil
+}
+
+// ex: 0 -> a1, 63 -> g8
+func IdxToCoordString(idx uint16) *string {
+	cs := fmt.Sprintf("%s%d", string(rune(idx % 8 + 'a')), idx / 8 + 1)
+	return &cs
+}
---
+++ engine/fen/fen.go
@@ -0,0 +1,141 @@
+package fen
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/geoffroyp2/chessgo/engine/constants"
+	"github.com/geoffroyp2/chessgo/engine/engineutils"
+	"github.com/geoffroyp2/chessgo/engine/position"
+)
+
+
+
+func PositionFromFEN(FEN *string) *position.Position {
+
+	newPos := position.NewPosition() // Empty position
+	sections := strings.Fields(*FEN) 
+
+	// Section 0: board description. Rows separated by '/'
+	var BBIdx = map[rune]uint32{
+		'K': constants.WHITEKING,
+		'k': constants.BLACKKING,
+		'Q': constants.WHITEQUEEN,
+		'q': constants.BLACKQUEEN,
+		'R': constants.WHITEROOK,
+		'r': constants.BLACKROOK,
+		'B': constants.WHITEBISHOP,
+		'b': constants.BLACKBISHOP,
+		'N': constants.WHITEKNIGHT,
+		'n': constants.BLACKKNIGHT,
+		'P': constants.WHITEPAWN,
+		'p': constants.BLACKPAWN,
+	}
+
+	row := 7
+	col := 0
+	for _, char := range sections[0] {
+		switch char {
+		case '/': 
+			col = 0
+			row--
+			break
+		case 'K':
+			fallthrough
+		case 'k':
+			fallthrough
+		case 'Q':
+			fallthrough
+		case 'q':
+			fallthrough
+		case 'B':
+			fallthrough
+		case 'b':
+			fallthrough
+		case 'N':
+			fallthrough
+		case 'n':
+			fallthrough
+		case 'R':
+			fallthrough
+		case 'r':
+			fallthrough
+		case 'P':
+			fallthrough
+		case 'p':
+			newPos.Pieces[BBIdx[char]] |= 1 << (row * 8 + col)
+			col++
+			break
+		case '1':
+			fallthrough
+		case '2':
+			fallthrough
+		case '3':
+			fallthrough
+		case '4':
+			fallthrough
+		case '5':
+			fallthrough
+		case '6':
+			fallthrough
+		case '7':
+			fallthrough
+		case '8':
+			charVal := int(char - '0')
+			col += charVal
+			break
+		default:
+			fmt.Println("Error: could not parse character ", string(char))
+			break
+		}
+	}
+	// Create union of all White & all Black pieces + occupied & empty sets
+	for idx := 0; idx < 6; idx++ {
+		newPos.AllWhite |= newPos.Pieces[idx]
+	}
+	for idx := 6; idx < 12; idx++ {
+		newPos.AllBlack |= newPos.Pieces[idx]
+	}
+	newPos.Occupied = newPos.AllWhite | newPos.AllBlack
+	newPos.Empty = ^newPos.Occupied
+
+	// Section 1: player turn
+	if sections[1] == "b" {
+		newPos.SetPlayerTurn(1)
+	}
+
+	// Section 2: castle rights
+	for idx, char := range sections[2] {
+		if char != '-' {
+			newPos.ToggleCastleRigth(uint16(idx))
+		}
+	}
+
+	// Section 3: En-passant square
+	val1, err := engineutils.CoordStringToIdx(&sections[3])
+	if err != nil {
+		newPos.SetEPSquare(constants.EPSQUARE_NONE)
+	} else {
+		newPos.SetEPSquare(val1)
+	}
+	
+
+	// Section 4: Half-move Clock
+	val2, err := strconv.Atoi(sections[4])
+	if err != nil {
+		fmt.Println("Could not parse Half-move counter: ", err)
+	} else {
+		newPos.HMClock = uint16(val2)
+	}
+	
+	// Section 5: Move number 
+	val2, err = strconv.Atoi(sections[5])
+	if err != nil {
+		fmt.Println("Could not parse Move Number: ", sections[5], err)
+	} else {
+		newPos.MoveNumber = uint32(val2)
+	}
+	
+	return newPos
+}
