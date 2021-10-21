c7828f98fa7e25f0f743d1f3d9318209c4ba7816
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-10-21 12:24:14 +0200

    Move generation init

--- api/getMove.go
+++ api/getMove.go
@@ -2,6 +2,10 @@ package api
 
 import (
 	"encoding/json"
+
+	"github.com/geoffroyp2/chessgo/engine/fen"
+	"github.com/geoffroyp2/chessgo/engine/move"
+	"github.com/geoffroyp2/chessgo/engine/movegen"
 )
 
 type MoveResponse struct {
@@ -10,6 +14,16 @@ type MoveResponse struct {
 }
 
 func getMove(FENString *string) *[]byte {
+
+	pos := fen.PositionFromFEN(FENString)
+	pos.PrintPosition()
+
+	var moveArray [400]uint32
+	moveAmount := movegen.GenerateMoves(pos, &moveArray)
+
+	for i := 0; i < moveAmount; i++ {
+		move.PrintMove(moveArray[i])
+	}
 
 	moveResponse := MoveResponse{FEN: "", Move: ""}
 	jsonRsponse, _ := json.Marshal(moveResponse)
---
+++ engine/movegen/movegen.go
@@ -0,0 +1,10 @@
+package movegen
+
+import "github.com/geoffroyp2/chessgo/engine/position"
+
+func GenerateMoves(pos *position.Position, moveArray *[400]uint32) int {
+
+	moveAmount := 0
+
+	return moveAmount
+}
