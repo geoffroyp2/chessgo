18d798a99f208e50fc05773a86e497217fc04424
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-10-23 13:12:32 +0200

    movegen now removes illegal moves

--- engine/movegen/movegen.go
+++ engine/movegen/movegen.go
@@ -2,6 +2,7 @@ package movegen
 
 import (
 	"github.com/geoffroyp2/chessgo/engine/constants"
+	"github.com/geoffroyp2/chessgo/engine/makemove"
 	"github.com/geoffroyp2/chessgo/engine/position"
 )
 
@@ -10,36 +11,50 @@ func GenerateMoves(pos *position.Position, moveArray *constants.MoveArray) int {
 
 	if pos.GetPlayerTurn() == constants.PLAYERWHITE {
 
-		ennemyAttacks := bQueenAttacks(pos)
-		ennemyAttacks |= bKingAttacks(pos)
-		ennemyAttacks |= bRookAttacks(pos)
-		ennemyAttacks |= bBishopAttacks(pos)
-		ennemyAttacks |= bKnightAttacks(pos)
-		ennemyAttacks |= bPawnAttacks(pos)
-
-		moveAmount = wKingMoves(pos, moveArray, moveAmount, ^ennemyAttacks)
 		moveAmount = wQueenMoves(pos, moveArray, moveAmount)
 		moveAmount = wRookMoves(pos, moveArray, moveAmount)
 		moveAmount = wBishopMoves(pos, moveArray, moveAmount)
 		moveAmount = wKnightMoves(pos, moveArray, moveAmount)
 		moveAmount = wPawnMoves(pos, moveArray, moveAmount)
+
+		// Check validity of moves and remove invalid ones
+		for i := 0; i < moveAmount; i++ {
+			posCopy := position.CopyPosition(pos)
+			makemove.MakeMove(posCopy, moveArray[i])
+			ennemyAttacks := getBAttacks(posCopy)
+			if ennemyAttacks & posCopy.Pieces[constants.WHITEKING] != 0 {
+				// move.PrintMove(moveArray[i])
+				moveArray[i] = moveArray[moveAmount - 1]
+				moveAmount--
+				i--
+			}
+		}
+		
+		ennemyAttacks := getBAttacks(pos)
+		moveAmount = wKingMoves(pos, moveArray, moveAmount, ^ennemyAttacks)
 		
 	} else {
 
-		ennemyAttacks := wQueenAttacks(pos)
-		ennemyAttacks |= wKingAttacks(pos)
-		ennemyAttacks |= wRookAttacks(pos)
-		ennemyAttacks |= wBishopAttacks(pos)
-		ennemyAttacks |= wKnightAttacks(pos)
-		ennemyAttacks |= wPawnAttacks(pos)
-		
-		moveAmount = bKingMoves(pos, moveArray, moveAmount, ^ennemyAttacks)
 		moveAmount = bQueenMoves(pos, moveArray, moveAmount)
 		moveAmount = bRookMoves(pos, moveArray, moveAmount)
 		moveAmount = bBishopMoves(pos, moveArray, moveAmount)
 		moveAmount = bKnightMoves(pos, moveArray, moveAmount)
 		moveAmount = bPawnMoves(pos, moveArray, moveAmount)
 
+		for i := 0; i < moveAmount; i++ {
+			posCopy := position.CopyPosition(pos)
+			makemove.MakeMove(posCopy, moveArray[i])
+			ennemyAttacks := getWAttacks(posCopy)
+			if ennemyAttacks & posCopy.Pieces[constants.BLACKKING] != 0 {
+				moveArray[i] = moveArray[moveAmount - 1]
+				moveAmount--
+				i--
+			}
+		}
+		
+		ennemyAttacks := getWAttacks(pos)
+		moveAmount = bKingMoves(pos, moveArray, moveAmount, ^ennemyAttacks)
+		
 	}
 
 	return moveAmount
@@ -62,3 +77,22 @@ func getBCapturedPiece(pos *position.Position, idx uint32) uint32 {
 	}
 	return constants.NULLPIECE
 }
+
+func getWAttacks(pos *position.Position) uint64 {
+	ennemyAttacks := wQueenAttacks(pos)
+	ennemyAttacks |= wKingAttacks(pos)
+	ennemyAttacks |= wRookAttacks(pos)
+	ennemyAttacks |= wBishopAttacks(pos)
+	ennemyAttacks |= wKnightAttacks(pos)
+	return ennemyAttacks | wPawnAttacks(pos)
+}
+
+func getBAttacks(pos *position.Position) uint64 {
+	ennemyAttacks := bQueenAttacks(pos)
+	ennemyAttacks |= bKingAttacks(pos)
+	ennemyAttacks |= bRookAttacks(pos)
+	ennemyAttacks |= bBishopAttacks(pos)
+	ennemyAttacks |= bKnightAttacks(pos)
+	return ennemyAttacks | bPawnAttacks(pos)
+}
+
