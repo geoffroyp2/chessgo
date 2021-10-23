fda71518194b5f549b1dd8f8128adbc17a2aaf14
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-10-23 13:12:57 +0200

    api now replies with a random move

--- api/getMove.go
+++ api/getMove.go
@@ -2,11 +2,12 @@ package api
 
 import (
 	"encoding/json"
+	"math/rand"
 
 	"github.com/geoffroyp2/chessgo/engine/constants"
-	"github.com/geoffroyp2/chessgo/engine/fen"
 	"github.com/geoffroyp2/chessgo/engine/move"
 	"github.com/geoffroyp2/chessgo/engine/movegen"
+	"github.com/geoffroyp2/chessgo/engine/stringutils/fen"
 )
 
 type MoveResponse struct {
@@ -22,14 +23,22 @@ func getMove(FENString *string) *[]byte {
 	var moveArray constants.MoveArray
 	moveAmount := movegen.GenerateMoves(pos, &moveArray)
 
-	for i := 0; i < moveAmount; i++ {
-		piece := move.MainPiece(moveArray[i]) 
-		if (piece == constants.BLACKKNIGHT || piece == constants.WHITEKNIGHT){
-			move.PrintMove(moveArray[i])
-		}
+	// for i := 0; i < moveAmount; i++ {
+	// 	// piece := move.MainPiece(moveArray[i]) 
+	// 	// if (piece == constants.BLACKKNIGHT || piece == constants.WHITEKNIGHT){
+	// 	// 	move.PrintMove(moveArray[i])
+	// 	// }
+	// 	// move.PrintMove(moveArray[i])
+	// }
+	
+	chosenMove := ""
+
+	if moveAmount > 0 {
+		randomIdx := rand.Int() % moveAmount
+		chosenMove = move.GetMoveString(moveArray[randomIdx])
 	}
 
-	moveResponse := MoveResponse{FEN: "", Move: ""}
+	moveResponse := MoveResponse{FEN: "", Move: chosenMove}
 	jsonRsponse, _ := json.Marshal(moveResponse)
 	
 	return &jsonRsponse
